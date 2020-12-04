6ad3804e97e1830e88f58b4a23e04b316df8dd6d
Chaya Maskeri Subraya <cms@leibniz-psychology.org>
2020-12-04 10:43:44 +0100

    Fix listing of workspaces with missing name

--- mashru3/cli.py
+++ mashru3/cli.py
@@ -1,15 +1,15 @@
 # Copyright 2019–2020 Leibniz Institute for Psychology
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in
 # all copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -452,7 +452,7 @@ def getPermissions (path: Path):
 
 def initWorkspace (ws, verbose=False):
 	# Fix permissions. Make sure the creator has default permissions, so files
-	# created by other users are accessible by default. 
+	# created by other users are accessible by default.
 	setPermissions (getuser (), 'rwX', ws.directory, default=True, recursive=True)
 
 	ws.ensureProfile ()
@@ -656,7 +656,7 @@ def dolist (args):
 						break
 
 				if not ignoreWorkspace:
-					formatResult (args, ws.toDict (), f'{ws.directory}: {ws.metadata["name"]}')
+					formatResult (args, ws.toDict (), f'{ws.directory}: {ws.metadata.get("name", "")}')
 			except InvalidWorkspace:
 				pass
 
